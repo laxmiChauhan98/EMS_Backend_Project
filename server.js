faa95c33040d0ff9a5fc8c3a408919ed7f62f2ba
laxmiChauhan98 <laxchauhan98@gmail.com>
2026-04-08 16:37:53 +0530

    Fix CORS for deployed frontend

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const employeeRoutes = require("./routes/employeeRoutes");
-
+const connectDB = require("./config/db");
 
 dotenv.config();
 
 const app = express();
-app.use(cors())
+
+app.use(cors({
+  origin: "https://ems-frontend-project.onrender.com",
+  methods: ["GET","POST","PUT","DELETE"],
+  credentials: true
+}));
+
+app.options("*", cors());
+
 app.use(express.json());
 
-app.use("/api/employees", employeeRoutes);
-console.log(employeeRoutes);
-const connectDB = require("./config/db");
-
 connectDB();
+
+app.use("/api/employees", employeeRoutes);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
